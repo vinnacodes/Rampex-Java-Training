8d94533e8528df1f568be49831fb774d514215bc
vinnacodes <vinnarasu2000@gmail.com>
2026-06-27 20:58:04 +0530

    nested loop to pratice nested loop scenarios

---
+++ src/Day6/class_Task/DoWhile.java
@@ -0,0 +1,18 @@
+package Day6.class_Task;
+
+public class DoWhile {
+
+
+    public static void main(String[] args) {
+
+
+        int i=1;
+        do{
+
+
+           System.out.print(i++);
+        }
+        while(i<=1);
+    }
+
+}
---
+++ src/Day6/class_Task/NestedLoopPractice.java
@@ -0,0 +1,32 @@
+package Day6.class_Task;
+
+public class NestedLoopPractice {
+
+
+    public static void main( String [] args){
+
+
+    for (int outer =1;outer <=5; outer++){
+
+
+
+        for (int inner =1 ; inner<=outer;  inner++){ //12345
+
+
+            System.out.print(inner);
+
+
+        }
+        System.out.println(); // takes to the next after the inner loop completely runs
+
+
+    }
+
+
+
+
+    }
+
+
+
+}
