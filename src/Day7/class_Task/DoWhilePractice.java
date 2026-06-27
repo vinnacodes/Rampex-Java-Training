f73f95b04e97e2aa2a899b17ac9800e3d31ee259
vinnacodes <vinnarasu2000@gmail.com>
2026-06-27 20:49:13 +0530

    Do whileloop practice

---
+++ src/Day7/class_Task/DoWhilePractice.java
@@ -0,0 +1,22 @@
+package Day7.class_Task;
+
+public class DoWhilePractice {
+
+
+
+    public static void main(String [] args){
+
+        int a =1;
+
+        do{
+
+
+            System.out.print( - a++ + " ");
+        }
+        while(a<=5);
+
+
+
+
+    }
+}
