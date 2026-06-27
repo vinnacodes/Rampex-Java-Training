dbd2f811d8f435d5c931d898ed2001914142ef61
vinnacodes <vinnarasu2000@gmail.com>
2026-06-27 15:24:56 +0530

    created a package for Arrays and to practice problems

---
+++ src/Day7/class_Task/Arrays/ArrayELementsPrinting.java
@@ -0,0 +1,19 @@
+package Day7.class_Task.Arrays;
+
+public class ArrayELementsPrinting {
+
+
+    public static void main(String[] args) {
+
+
+
+        int [] nums ={4,5,6,7,8,9,10};
+
+        //System.out.println(arr.length);
+
+        for(int i=0;i<nums.length;i++){
+
+            System.out.println(nums[i]);
+        }
+    }
+}
