66cb486c733d013e114f2bcadca5db3012d52b65
vinnacodes <vinnarasu2000@gmail.com>
2026-06-29 19:34:57 +0530

    String accessing the character index , traversing the string using loop

---
+++ src/Day8/Class_Task/String/Strings.java
@@ -0,0 +1,37 @@
+package Day8.Class_Task.String;
+
+public class Strings {
+
+
+   public static void main(String []a){
+
+
+       char [] letters ={'a','n','u'};
+
+
+       String name = "anu";
+
+       String name1 = new String("anu");
+
+
+      // System.out.println(name.charAt(2));
+
+       int n =name.length();
+
+       for(int i =n-1;i>=0;i--){
+
+           System.out.print(name.charAt(i));
+
+
+
+
+       }
+
+
+
+
+
+
+   }
+
+}
