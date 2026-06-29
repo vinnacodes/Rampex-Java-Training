3c6f919ea45197ea5b0aa4414b69a6a6fa4bdf2b
vinnacodes <vinnarasu2000@gmail.com>
2026-06-29 19:30:33 +0530

    pattern printing java

---
+++ src/Day6/class_Task/PatternPrinting.java
@@ -0,0 +1,23 @@
+package Day6.class_Task;
+
+public class PatternPrinting {
+
+
+    public static void main(String [] args ){
+
+for (int i=5;i>=1;i--)
+        {
+            for (int j = 1; j <= i; j++) { // prints * * * * *
+
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+
+
+        }
+    }
+
+
+}
