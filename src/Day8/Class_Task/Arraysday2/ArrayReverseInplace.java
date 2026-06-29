9dbeabcb3e4018007d7be729977211224f1b4c31
vinnacodes <vinnarasu2000@gmail.com>
2026-06-29 19:23:17 +0530

    Inplace Array reversal by accessing index using index

---
+++ src/Day8/Class_Task/Arraysday2/ArrayReverseInplace.java
@@ -0,0 +1,36 @@
+package Day8.Class_Task.Arraysday2;
+
+import java.util.Arrays;
+public class ArrayReverseInplace {
+
+
+    public static void main(String[] args) {
+
+
+        int [] nums ={1,2,3,4};
+
+        System.out.println(Arrays.toString(nums));
+        int temp =0;
+        int n =nums.length;
+
+       for (int i=0;i<nums.length/2;i++){
+
+
+           temp = nums[i];
+
+           nums[i]=nums[n-1-i];
+           nums[n-1-i]=temp;
+
+       }
+       System.out.println(Arrays.toString(nums));
+
+
+
+
+
+
+
+
+
+    }
+}
