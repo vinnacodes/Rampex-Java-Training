18f92225fe3f4103bc7a281839677a686f2d320d
vinnacodes <vinnarasu2000@gmail.com>
2026-06-29 19:33:29 +0530

    schedule using nested loop

--- src/Day6/class_Task/Schedule.java
+++ src/Day6/class_Task/Schedule.java
@@ -11,11 +11,15 @@ public class Schedule {
 for(int month =1;month <=12;month++)
 
 
-        {
+       // {  if (month == 2){break;}
             for (int week = 1; week <= 4; week++) { //one month loop
 
 
+if (month ==2){continue;}
+
                 for (int day = 1; day <= 7; day++) { // one week loop
+
+
 
                     System.out.println(  "month "+ month+" week" + week + " day " + day +  "days completed :"+ ++daysCompleted);
                     System.out.println(" come to college " +
@@ -35,4 +39,4 @@ for(int month =1;month <=12;month++)
 
 
     }
-}
+
