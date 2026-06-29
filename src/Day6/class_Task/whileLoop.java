0ee4e2f1f298ccab7ea74809d6770c3d2a63b03b
vinnacodes <vinnarasu2000@gmail.com>
2026-06-29 19:45:40 +0530

    using while loop to track variable using the iteration

---
+++ src/Day6/class_Task/whileLoop.java
@@ -0,0 +1,28 @@
+package Day6.class_Task;
+import  java.util.Scanner;
+public class whileLoop {
+
+
+
+    public static void main(String [] args){
+
+Scanner sc = new Scanner(System.in);
+
+
+        int pass =1234;
+
+        int userPass =0;
+
+
+        while (pass!=userPass){
+
+System.out.println("please enter your pin : " );
+   userPass =sc.nextInt();
+
+
+
+        }
+System.out.print("welcome");
+
+    }
+}
