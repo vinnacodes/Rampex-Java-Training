b0e78ad92b5bd64008e16a9b9ec9326c68eba509
vinnacodes <vinnarasu2000@gmail.com>
2026-06-29 19:42:10 +0530

    find the sum of the Array using loop and Accum

---
+++ src/Day7/class_Task/Arrays/SumOfTheArray.java
@@ -0,0 +1,26 @@
+package Day7.class_Task.Arrays;
+
+public class SumOfTheArray {
+
+    public static void main(String [] args){
+
+        int[] arr = {1,2,3,4,5};
+
+
+int sum=0;
+
+        for (int i=0;i<5;i++){ //> 0,2,4
+
+
+
+               sum =sum +arr[i];
+
+
+        }
+        System.out.println(sum);
+
+
+    }
+}
+// Task : products of elements in the Array
+// Task : sum of values in the even positions(indices)
