b826076fd6bfd0f9a3b5af384723abf2405a0d82
vinnacodes <vinnarasu2000@gmail.com>
2026-07-02 00:30:47 +0530

    java built in methods

---
+++ src/Day9/class_Task/ArraysPractice/String/BuiltInMethods.java
@@ -0,0 +1,23 @@
+package Day9.class_Task.ArraysPractice.String;
+
+public class BuiltInMethods {
+
+
+
+    public static void main(String [] args){
+
+
+
+       String  name ="Mazza";
+
+//       System.out.println(name.toUpperCase());
+//       System.out.println(name);
+//       System.out.println(name.replaceAll("M","L"));
+
+        System.out.println(name.equalsIgnoreCase("mazza"));
+
+
+
+
+    }
+}
